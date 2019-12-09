4af18720250526a755b6cbed7dfb72d23141664e
Christophe Nanteuil <christophe.nanteuil@gmail.com>
2019-12-09 23:44:55 +0100

    Add -s and -F options.

--- pogrep.py
+++ pogrep.py
@@ -6,10 +6,8 @@ __version__ = "0.1.1"
 
 import argparse
 import curses
-from glob import glob
 import sys
 from textwrap import fill
-
 import regex
 import polib
 from tabulate import tabulate
@@ -25,7 +23,7 @@ def get_colors():
         green = str(curses.tparm(fg_color, 2), "ascii")
         magenta = str(curses.tparm(fg_color, 5), "ascii")
         no_color = str(curses.tigetstr("sgr0"), "ascii")
-    except Exception:
+    except curses.error:
         red, green, magenta = "", "", ""
         no_color = ""
     return red, green, magenta, no_color
@@ -59,25 +57,26 @@ def colorize(text, pattern, prefixes):
     return result
 
 
-def find_in_po(pattern, path, linenum, file_match):
+def find_in_po(pattern, path, linenum, file_match, no_messages):
     table = []
     prefixes = []
     for file in path[0]:
         try:
             pofile = polib.pofile(file.name)
         except OSError:
-            print("{} doesn't seem to be a .po file".format(file), file=sys.stderr)
+            if not no_messages:
+                print("{} doesn't seem to be a .po file".format(file.name), file=sys.stderr)
             continue
         for entry in pofile:
             if entry.msgstr and regex.search(pattern, entry.msgid):
                 if file_match:
-                    print(MAGENTA + file + NO_COLOR)
+                    print(MAGENTA + file.name + NO_COLOR)
                     break
                 left = entry.msgid
                 if linenum:
                     pnum = str(entry.linenum) + ":"
                     if len(path[0]) > 1:
-                        pfile = file + ":"
+                        pfile = file.name + ":"
                     else:
                         pfile = ""
                     left = pfile + pnum + left
@@ -94,6 +93,8 @@ def find_in_po(pattern, path, linenum, file_match):
 
 def parse_args():
     parser = argparse.ArgumentParser(description="Find translated words.")
+    parser.add_argument("-F", "--fixed-strings", action="store_true",
+                        help="Interpret pattern as fixed string, not regular expressions")
     parser.add_argument("-i", "--ignore-case", action="store_true",
                         help="Ignore case distinctions, so that characters that differ only in case match each other.")
     parser.add_argument("-w", "--word-regexp", action="store_true",
@@ -103,6 +104,8 @@ def parse_args():
     parser.add_argument("-l", "--files-with-matches", action="store_true",
                         help="Suppress normal output; instead print the name of each input file from which output "
                              "would normally have been printed.  The scanning will stop on the first match.")
+    parser.add_argument("-s", "--no-messages", action="store_true",
+                        help="Suppress error messages about nonexistent or unreadable files.")
     parser.add_argument("pattern")
     parser.add_argument("path", action="append", type=argparse.FileType('r'), nargs='+')
     return parser.parse_args()
@@ -110,11 +113,14 @@ def parse_args():
 
 def main():
     args = parse_args()
+    if args.fixed_strings:
+        for car in "\.^$*+?{}[]|()":
+            args.pattern = args.pattern.replace(car, "\\" + car)
     if args.word_regexp:
         args.pattern = r"\b" + args.pattern + r"\b"
     if args.ignore_case:
         args.pattern = r"(?i)" + args.pattern
-    find_in_po(args.pattern, args.path, args.line_number, args.files_with_matches)
+    find_in_po(args.pattern, args.path, args.line_number, args.files_with_matches, args.no_messages)
 
 
 if __name__ == "__main__":
