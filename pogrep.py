820ea77e10b193f0be616aa194cdec31581b6f5e
Julien Palard <julien@palard.fr>
2020-01-07 14:07:59 +0100

    Use get_terminal_size from stdlib.

--- pogrep.py
+++ pogrep.py
@@ -13,6 +13,7 @@ from textwrap import fill
 import regex
 import polib
 from tabulate import tabulate
+from shutil import get_terminal_size
 
 
 def get_colors():
@@ -34,16 +35,6 @@ def get_colors():
 RED, GREEN, MAGENTA, NO_COLOR = get_colors()
 
 
-def get_term_width():
-    scr = curses.initscr()
-    width = scr.getmaxyx()[1]
-    try:
-        curses.endwin()
-    except curses.error:
-        pass
-    return width
-
-
 def colorize(text, pattern, prefixes):
     result = regex.sub(pattern, RED + r"\g<0>" + NO_COLOR, text)
     for pnum, pfile in prefixes:
@@ -59,7 +50,7 @@ def colorize(text, pattern, prefixes):
 def find_in_po(pattern, path, linenum, file_match, no_messages):
     table = []
     prefixes = []
-    term_width = get_term_width()
+    term_width = get_terminal_size()[0]
     for filename in path:
         try:
             pofile = polib.pofile(filename)
